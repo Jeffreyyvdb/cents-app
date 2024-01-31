d38bc3040cedff9a52c5b36a0fb1e12eb0998173
Jeffrey van den Brink <j.vandenbrink@tignl.eu>
2024-01-31 22:44:12 +0100

    init shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,14 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>
+	import { ModeWatcher } from 'mode-watcher';
+	import '../app.pcss';
+</script>
+
+<ModeWatcher />
+
+<div class="bg-background relative flex min-h-screen flex-col" id="page" data-vaul-drawer-wrapper>
+	<!-- <SiteHeader /> -->
+	<div class="flex-1">
+		<slot />
+	</div>
+	<!-- <SiteFooter /> -->
+</div>
