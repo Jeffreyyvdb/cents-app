c1b6f75d54574e391fb53df30720ae1b7f42d9ea
Jeffrey van den Brink <j.vandenbrink@tignl.eu>
2024-01-31 23:36:44 +0100

    Stole a navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script>
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.pcss';
+	import Navigation from '$lib/components/navigation/navigation.svelte';
+
 </script>
 
 <ModeWatcher />
 
 <div class="relative flex min-h-screen flex-col" id="page" data-vaul-drawer-wrapper>
-	<!-- <SiteHeader /> -->
+	<Navigation/>
 	<div class="mt-8 md:mt-12">
 		<slot />
 	</div>
