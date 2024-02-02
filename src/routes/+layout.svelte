e57e00af9cc0638015df77d52943e9661f6e968d
Jeffrey van den Brink <j.vandenbrink@tignl.eu>
2024-02-02 14:08:21 +0100

    add speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import SiteHeader from '$lib/components/site-header.svelte';
 	import SiteFooter from '$lib/components/site-footer.svelte';
 	import { dev } from '$app/environment';
+	import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
 	import { inject } from '@vercel/analytics';
 
 	inject({ mode: dev ? 'development' : 'production' });
