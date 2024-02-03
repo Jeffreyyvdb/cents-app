54e864b96ca58cf3f13fb43b3bc2fdd609ba7376
Jeffrey van den Brink <j.vandenbrink@tignl.eu>
2024-02-04 00:13:09 +0100

    even better auth experience

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -2,15 +2,26 @@
 	import { dev } from '$app/environment';
 	import * as Form from '$lib/components/ui/form';
 	import { type FormOptions } from 'formsnap';
+	import { Reload } from 'radix-icons-svelte';
 	import { toast } from 'svelte-sonner';
-	import type { ActionData, PageData } from './$types';
+	import type { PageData } from './$types';
 	import { loginSchema as schema } from './schema';
+	import { ExclamationTriangle } from 'radix-icons-svelte';
+	import * as Alert from '$lib/components/ui/alert';
+	import type { ActionResult } from '@sveltejs/kit';
 
 	export let data: PageData;
 
+	let loginResult: ActionResult;
+	let loading = false;
+
 	const options: FormOptions<typeof schema> = {
+		onSubmit(input) {
+			loading = true;
+		},
 		onResult: ({ result }) => {
-			console.log('result', result);
+			loginResult = result;
+			loading = false;
 			if (result.type === 'failure') {
 				toast('Login failed', {
 					description: result.data.message
@@ -20,28 +31,39 @@
 	};
 </script>
 
-<Form.Root
-	class="m-auto max-w-md"
-	method="POST"
-	form={data.form}
-	{schema}
-	let:config
-	debug={dev}
-	{options}
->
-	<Form.Field {config} name="email">
-		<Form.Item>
-			<Form.Label>Email</Form.Label>
-			<Form.Input />
-			<Form.Validation />
-		</Form.Item>
-	</Form.Field>
-	<Form.Field {config} name="password">
-		<Form.Item>
-			<Form.Label>Password</Form.Label>
-			<Form.Input type="password" />
-			<Form.Validation />
-		</Form.Item>
-	</Form.Field>
-	<Form.Button>Login</Form.Button>
-</Form.Root>
+<section class="m-auto max-w-md">
+	<h1 class="mb-4 text-center font-title text-4xl leading-none md:text-5xl lg:text-6xl">
+		Login to ¢ents
+	</h1>
+	<Form.Root method="POST" form={data.form} {schema} let:config debug={dev} {options}>
+		<Form.Field {config} name="email">
+			<Form.Item>
+				<Form.Label>Email</Form.Label>
+				<Form.Input />
+				<Form.Validation />
+			</Form.Item>
+		</Form.Field>
+		<Form.Field {config} name="password">
+			<Form.Item>
+				<Form.Label>Password</Form.Label>
+				<Form.Input type="password" />
+				<Form.Validation />
+			</Form.Item>
+		</Form.Field>
+		{#if loading}
+			<Form.Button disabled class="my-2 w-full">
+				<Reload class="mr-2 h-4 w-4 animate-spin" />
+				Please wait
+			</Form.Button>
+		{:else}
+			<Form.Button class="my-2 w-full">Login</Form.Button>
+		{/if}
+
+		{#if loginResult?.type === 'failure'}
+			<Alert.Root variant="destructive">
+				<ExclamationTriangle class="h-4 w-4" />
+				<Alert.Title>{loginResult?.data?.message}</Alert.Title>
+			</Alert.Root>
+		{/if}
+	</Form.Root>
+</section>
